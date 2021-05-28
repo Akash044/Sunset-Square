fd3747ef7315c800b01e74392e7ea90408530746
Md. Akash Hossain <akash.cse.ruet@gmail.com>
2021-05-28 23:37:08 +0600

    changed app component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi</div>;
+  return <div className="App">this is akash</div>;
 }
 
 export default App;
