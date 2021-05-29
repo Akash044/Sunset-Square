7d2fa2ac71daf11d6d1f1dfebbc2af9dd79c28aa
Farjana2345 <fa8547830@gmail.com>
2021-05-29 13:40:25 +0200

    created home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./component/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      this is Shimu
-      <h1>So frustr </h1>
-      <h1>Akash vai rocks</h1>
-      <h1>ভালো লাগে নারে ভাই</h1>
+      <Home></Home>
     </div>
   );
 }
