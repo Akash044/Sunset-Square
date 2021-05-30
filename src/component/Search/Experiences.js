85cfb85b2564e42fa51c83299d5ac269ad74e362
Md. Akash Hossain <akash.cse.ruet@gmail.com>
2021-05-30 14:55:48 +0600

    experiences add

---
+++ src/component/Search/Experiences.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Stars from "../../stars/Stars";
+
+const Experiences = (props) => {
+  const { location, name, price, person, Features, image, host, review } =
+    props.experience;
+
+  return (
+    <div className="col-md-4 col-sm-12">
+      <div className="card mx-auto" style={{ width: "16rem",marginBottom:"10px" }}>
+        <img src={image} className="card-img-top" alt="..." />
+        <div className="card-body">
+        <h6 className="card-text">{location}</h6>
+          <h4 className="card-title">{name}</h4>
+          <p>{`$${price} per person`}</p>
+          
+          <p>
+          <Stars />
+          <Stars />
+          <Stars />
+          <Stars />
+          {`   ${review[0].count}`}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experiences;
